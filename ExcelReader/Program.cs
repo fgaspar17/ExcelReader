9b76c65c2c1323864f53c2c811db7f0220f8c08f
Francisco Gaspar Serón <nectryk@gmail.com>
2024-10-18 18:06:38 +0200

    Add project files.

---
+++ ExcelReader/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+FileInfo newFile = new FileInfo("C:\\Users\\ErNaN\\Downloads\\SaleData.xlsx");
+
+ExcelPackage pck = new ExcelPackage(newFile);
